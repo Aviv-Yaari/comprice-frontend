d67f3bfc02cece731cc846fd72547fcfab84db3b
Aviv-Yaari <avivyar@gmail.com>
2022-10-15 23:01:33 +0300

    Improve filter

--- src/components/TableFilter/TableFilter.tsx
+++ src/components/TableFilter/TableFilter.tsx
@@ -1,11 +1,28 @@
-import React from 'react';
+import React, { useState } from 'react';
 import FilterIcon from '@mui/icons-material/FilterList';
-import IconButton from '@mui/material/IconButton';
+import Button from '@mui/material/Button';
+import TextField from '@mui/material/TextField';
+import { Box } from '@mui/material';
 
 const TableFilter: React.FC = () => {
+  const [filterOpen, setIsFilterOpen] = useState(false);
+  const filtersText = filterOpen ? 'Hide filters' : 'Show filters';
+
+  // eslint-disable-next-line @typescript-eslint/explicit-function-return-type
+  const toggleFilter = () => {
+    setIsFilterOpen(filterOpen => !filterOpen);
+  };
+
   return (
     <section>
-      <IconButton aria-label='Filters'><FilterIcon /></IconButton>
+      <Button aria-label={filtersText} startIcon={<FilterIcon />} onClick={toggleFilter}>{filtersText}</Button>
+      {filterOpen && (
+        <Box display='flex' flexDirection='column' gap='1em'>
+          <TextField variant='outlined' label="Name"/>
+          <TextField variant='outlined' label="Price"/>
+          <TextField variant='outlined' label="Mashu"/>
+        </Box>
+      )}
     </section>
   );
 };
