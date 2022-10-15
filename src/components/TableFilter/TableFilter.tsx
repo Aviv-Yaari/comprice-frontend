3f5a8c9a3d868d017cecf0493da0ab004fef5a55
Aviv-Yaari <avivyar@gmail.com>
2022-10-16 01:25:13 +0300

    Improve filters

--- src/components/TableFilter/TableFilter.tsx
+++ src/components/TableFilter/TableFilter.tsx
@@ -3,6 +3,31 @@ import FilterIcon from '@mui/icons-material/FilterList';
 import Button from '@mui/material/Button';
 import TextField from '@mui/material/TextField';
 import { Autocomplete, Box, FormControl, InputLabel, ListSubheader, MenuItem, Select } from '@mui/material';
+import products from '../../mocks/products.json';
+import userSettings from '../../mocks/user_settings.json';
+import Product from '../../types/product';
+
+interface ProductNameAndList {
+  label?: string
+  list: string
+}
+
+function findListForProduct (lists: Record<string, string[]>, itemCode?: string): string {
+  for (const listName in lists) {
+    // eslint-disable-next-line @typescript-eslint/strict-boolean-expressions
+    if (itemCode && Object.prototype.hasOwnProperty.call(lists, listName)) {
+      const itemCodes = lists[listName];
+      if (itemCodes.includes(itemCode)) {
+        return listName;
+      }
+    }
+  }
+  return 'No list';
+}
+
+function addListFieldToProducts (products: Product[]): ProductNameAndList[] {
+  return products.map(product => ({ label: product.item_name, list: findListForProduct(userSettings.lists, product.item_code) }));
+}
 
 const TableFilter: React.FC = () => {
   const [filterOpen, setIsFilterOpen] = useState(true);
@@ -20,16 +45,16 @@ const TableFilter: React.FC = () => {
         <Box display='flex' flexDirection='column' gap='1em' width={600}>
           <Box display='flex' gap='1em' alignItems='center'>
             <Autocomplete
-             options={[{ label: 'List 1', group: 'Lists' }, { label: 'Product 1', group: 'Other products' }]}
+             options={addListFieldToProducts(products).sort((a, b) => -b.list.localeCompare(a.list))}
              disablePortal
              renderInput={(params) => <TextField {...params} label="Name" />}
              renderOption={(props, option) => (
                <Box display='flex' component="li" {...props}>
                  <span style={{ flexGrow: 1 }}>{option.label}</span>
-                 {option.group !== 'Other products' && <Button>Remove</Button>}
+                 {option.list !== 'No list' && <Button>Remove</Button>}
                </Box>)}
              multiple
-             groupBy={(option) => option.group}
+             groupBy={(option) => option.list}
              sx={{ flexGrow: 1 }}
               />
             <Button>Create list</Button>
