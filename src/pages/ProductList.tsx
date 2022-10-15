9fd083b6508afa2e5cfa337c615c56ee861dda50
Aviv <avivyar@gmail.com>
2022-10-15 22:01:41 +0300

    Add product table pagination

--- src/pages/ProductList.tsx
+++ src/pages/ProductList.tsx
@@ -15,9 +15,13 @@ import KeyboardArrowUpIcon from '@mui/icons-material/KeyboardArrowUp';
 import { mockDelay } from '../utils/generalUtils';
 import Product from '../types/product';
 import productsMock from '../mocks/products.json';
+import { TablePagination } from '@mui/material';
+import TableFilter from '../components/TableFilter/TableFilter';
 
 const ProductList: React.FC = () => {
   const [products, setProducts] = useState<Product[] | null>();
+  const [page, setPage] = useState(0);
+  const [rowsPerPage, setRowsPerPage] = useState(5);
   useEffect(() => {
     mockDelay(2000).then(() => setProducts(productsMock)).catch(() => {});
   }, []);
@@ -25,6 +29,7 @@ const ProductList: React.FC = () => {
   return (
     <>
       <h2>Products</h2>
+      <TableFilter />
       <TableContainer component={Paper}>
         <Table aria-label="collapsible table">
           <TableHead>
@@ -36,12 +41,21 @@ const ProductList: React.FC = () => {
             </TableRow>
           </TableHead>
           <TableBody>
-            {products?.map((product) => (
+            {products?.slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage).map((product) => (
               <Row key={product.id} product={product} />
             ))}
           </TableBody>
         </Table>
       </TableContainer>
+      <TablePagination
+          rowsPerPageOptions={[5, 10, 25]}
+          component="div"
+          count={products?.length ?? 0}
+          rowsPerPage={rowsPerPage}
+          page={page}
+          onPageChange={(ev, newPage) => setPage(newPage)}
+          onRowsPerPageChange={(ev) => { setRowsPerPage(parseInt(ev.target.value, 10)); setPage(0); }}
+        />
     </>
   );
 };
